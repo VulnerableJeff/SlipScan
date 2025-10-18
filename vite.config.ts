e126ee6bc98981f353f895bdb864565dc30f10c0
VulnerableJeff <miagomez51122@gmail.com>
2025-10-18 12:54:59 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+// TODO: change to your actual repo name
+const REPO = process.env.VITE_REPO_NAME || 'slipscan';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/slipscan/', // replace with your repo name
+  base: `/${REPO}/`,
 })
