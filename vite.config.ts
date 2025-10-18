a271e4ac2a91eab91617ff56c10972431e1d252b
VulnerableJeff <miagomez51122@gmail.com>
2025-10-18 13:06:33 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// TODO: change to your actual repo name
-const REPO = process.env.VITE_REPO_NAME || 'slipscan';
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${REPO}/`,
+  base: '/SlipScan/',    // <<— IMPORTANT for GitHub Pages
 })
